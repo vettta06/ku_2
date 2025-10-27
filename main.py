ddb57247f58d171b8fe37fbc379459fa15b60e68
vettta06 <kuznecovaElizaveta2006@yandex.ru>
2025-10-27 15:17:29 +0300

    task 4 of 1

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import argparse
+import os
+import sys
+
+
+def validate_args(args):
+    errors = []
+    if not args.package:
+        errors.append("Empty name")
+    if not args.repo:
+        errors.append("Empty path")
+    else:
+        if not (args.repo.startswith("http://") or args.repo.startswith("https://") or os.path.isfile(args.repo)):
+            errors.append("Path not exist")
+    if args.mode not in ["online", "test"]:
+        errors.append(f"{args.mode} mode not exist")
+    if not args.version:
+        errors.append("Empty version")
+    if not args.output:
+        errors.append("Empty output name")
+    if args.depth <= 0:
+        errors.append("Depth incorrect")
+    return errors
 
 
 def main():
@@ -12,6 +34,11 @@ def main():
     parser.add_argument("--depth", type=int, default=3, help="Max dependency depth")
 
     args = parser.parse_args()
+    errors = validate_args(args)
+    if errors:
+        for error in errors:
+            print(error)
+        sys.exit(1)
 
     print(f"package: {args.package}")
     print(f"repo: {args.repo}")
